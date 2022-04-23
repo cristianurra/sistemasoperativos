81a592aaa59936227e91c5d191c5607f75c08268
Cristhina Urra Aldunate <40528744+cristianurra@users.noreply.github.com>
2022-04-22 23:59:03 -0400

    Update tarea1.c

--- tarea1.c
+++ tarea1.c
@@ -27,10 +27,13 @@ int main(void)
 	 
 	 if(pidC>0) //Esto se ejecuta solo en el proceso padre
 		{	
+		int SMOBJ_SIZE=900;	
+			
 		int fd1;
 		fd1 = shm_open(MEMORIA1 , O_CREAT | O_RDWR, 00600);
 		int fd2;
 		fd2 = shm_open(MEMORIA2 , O_CREAT | O_RDWR, 00600);	
+		
 		
 		if(fd1 == -1){
 			printf("hubo un error al crear memoria compartida 1\n");
@@ -39,27 +42,88 @@ int main(void)
 		if(fd2 == -1){
 			printf("hubo un error al crear memoria compartida 2\n");
 			exit(1);
-		}
-		 
-		 
-		 
-		int SMOBJ_SIZE=n*12;
-		int k=0;
+		}			
+
+		if(-1 == ftruncate(fd1, SMOBJ_SIZE)){
+			printf("la memoria compartida no pudo ser rezsizeada \n");
+			}			
+		if(-1 == ftruncate(fd2, SMOBJ_SIZE)){
+			printf("la memoria compartida no pudo ser rezsizeada \n");
+			}				
+			
+			
+		
+		int k1=0;
 		srand(time(NULL)); 					//Una semilla que varía, para mas "aleatoriedad"
-		while(k<n)
+		while(k1<n)
 		{
 			int j = rand()%n;
 			printf("Proceso padre %d ha generado: %d\n",pidC, j);
-			/*sleep(0.2);*/
-			k=k+1;
+			int fda;
+			char bufa[]="proceso escrito\n";
+			char *ptra;
+			fda = shm_open(MEMORIA1 , O_RDWR, 0);
+			
+			if(fda == -1){
+			printf("hubo un error :c \n");
+			exit(1);
+			}
+			
+			ptra = mmap(0, sizeof(bufa), PROT_WRITE, MAP_SHARED,fda,0);
+			if (ptra==MAP_FAILED){
+				printf("Error mapeando \n");
+				exit(1);
+			}
+			memcpy(ptra,bufa,sizeof(bufa));
+			close(fda);	
+			k1=k1+1;
 		}
 		
+		int k2=0;
+		srand(time(NULL)); 					//Una semilla que varía, para mas "aleatoriedad"
+		while(k2<n)
+		{
+			int j = rand()%n;
+			printf("Proceso padreaaaa %d ha generado: %d\n",pidC, j);
+			int fdb;
+			char bufb[]="proceso escrito\n";
+			char *ptrb;
+			//printf("%d\n",ptrb);
+			fdb = shm_open(MEMORIA2 , O_RDWR, 0);
+			
+			if(fdb == -1){
+			printf("hubo un error :c \n");
+			exit(1);
+			}
+			
+			ptrb = mmap(0, sizeof(bufb), PROT_WRITE, MAP_SHARED,fdb,0);
+			if (ptrb==MAP_FAILED){
+				printf("Error mapeando \n");
+				exit(1);
+			}
+			memcpy(ptrb,bufb,sizeof(bufb));
+			close(fdb);	
+			k2=k2+1;
+		}		
 		
-		
-		
-		
+	
 		
 	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 	 else if(pidC == 0)//Esto se ejecuta solo en el proceso hijo
 	 { 
 		 
