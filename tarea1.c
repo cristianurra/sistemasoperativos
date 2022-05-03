fb67488f153c943cf19f0499993e0a4ec8ca5a9d
seewateriv <104654943+seewateriv@users.noreply.github.com>
2022-05-02 21:22:48 -0400

    Update tarea1.c

--- tarea1.c
+++ tarea1.c
@@ -13,6 +13,9 @@
 
 void switcha(int columna,int size);
 void leer_posix(int columna,int size);
+void leer_posix2(int columna,int size);
+int particion (int izquierda, int derecha, int size);
+void quicksort(int izquierda,int derecha,int size);
 
 #define MEMORIA1 "/urra_memoria1"			//Se crearan dos memorias compartidas posix
 #define MEMORIA2 "/urra_memoria2"
@@ -166,6 +169,13 @@ int main(void)
 	printf("\nProceso padre finalizado\n"); 
 	 return 0; 
  }
+
+
+
+ /* ======================================================================*/
+ /* ====================== FUNCIONES AUXILIARES ==========================*/
+ /* ======================================================================*/
+
  
 void switcha(int columna,int size)	//compara dos valores consecutivos
 {
@@ -261,3 +271,117 @@ void leer_posix(int columna,int size)	//compara dos valores consecutivos
 		printf("%d, ",aa);
 		close(fd);
 }
+
+
+void leer_posix2(int columna,int size)
+{
+		int fd;
+		char *ptr1;
+		char *ptr2;
+		char bufaa[size];
+		char bufbb[size];
+		struct stat shmobj_st;	
+		fd = shm_open(MEMORIA2, O_RDWR,0);
+
+		
+		if(fd == -1){
+			printf("Error fd %s\n", strerror(errno));
+			exit(1);
+			}
+			
+		if(fstat(fd, &shmobj_st)==1)
+			{
+			printf("error fstat \n");
+			exit(1);
+			}
+		
+		ptr1=mmap(NULL,shmobj_st.st_size, PROT_READ, MAP_SHARED,fd,0)+columna*size;
+		
+		if(ptr1==MAP_FAILED)
+		{
+			printf("Fallo el proceso de mapeo leyendo el proceso %s\n",strerror(errno));
+			exit(1);
+		}
+		
+		char *a=ptr1;
+		int aa =  atoi(a);
+
+		printf("%d, ",aa);
+		close(fd);
+}
+
+int particion (int izquierda, int derecha, int size){		// esta funcion es el núclero del quicksort:
+      		int fd;						// compara los elementos del conjunto recorriendo desde los extremos
+	  	char *ptr_izq;					// e intercambiando las posiciones según corresponda
+	  	char *ptr_der;
+	  	char bufaa[size];
+	  	char bufbb[size];
+	  	struct stat shmobj_st;
+  		fd = shm_open(MEMORIA2, O_RDWR,0);
+     		if(fd == -1){
+			  printf("Error fd %s\n", strerror(errno));
+			  exit(1);
+		}
+		if(fstat(fd, &shmobj_st)==1){
+			  printf("error fstat \n");
+		  	  exit(1);
+		}
+      
+ 		ptr_izq=mmap(NULL,shmobj_st.st_size, PROT_READ, MAP_SHARED,fd,0)+izquierda*size;
+		ptr_der=mmap(NULL,shmobj_st.st_size, PROT_READ, MAP_SHARED,fd,0)+derecha*size;	
+	  	if(ptr_izq==MAP_FAILED)
+	  	{
+		 	printf("Fallo el proceso de mapeo leyendo el proceso %s\n",strerror(errno));
+		  	exit(1);
+	  	}
+		  
+		char *a=ptr_izq;
+		char *b=ptr_der;
+		int vizq =  atoi(a);
+	  	int vder = atoi(b);
+      		int pivote = vizq;
+      
+      		while (1){
+
+        		ptr_izq=mmap(0,size, PROT_WRITE, MAP_SHARED,fd,0)+izquierda*size;
+		    	ptr_der=mmap(0,size, PROT_WRITE, MAP_SHARED,fd,0)+derecha*size;
+		    	char *a=ptr_izq;
+		    	char *b=ptr_der;
+	  	  	vizq =  atoi(a);
+	    		vder = atoi(b);
+        		pivote = vizq;
+  		  
+        		if (vizq < pivote){
+          			izquierda++;
+        		}
+        		if (vder > pivote){
+          			derecha--;
+        		}
+        		if (izquierda >= derecha){
+          			return derecha;  
+        		} 
+        		else {
+          
+          			sprintf(bufaa,"%d\n",vizq);
+			   	sprintf(bufbb,"%d\n",vder);			
+		    		memcpy(ptr_izq,bufbb,sizeof(bufbb));
+			   	memcpy(ptr_der,bufaa,sizeof(bufaa));
+			    	//close(fd);
+          			izquierda++;
+          			derecha--;
+          		}
+       		}
+       		close (fd);
+}   
+
+
+void quicksort(int izquierda,int derecha,int size){	// compara e intercambia los elementos del conjunto mediante el uso de la funcion particion
+							// y lo vuelve a hacer recursivamente seccionando el conjunto.
+    if (izquierda < derecha){
+      int indice = particion(izquierda, derecha, size);
+      quicksort(izquierda,indice,size);
+      quicksort(indice+1,derecha,size);
+      }
+}
+
+
